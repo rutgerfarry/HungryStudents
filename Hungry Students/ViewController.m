ef75822dcca83ff2edba29d120ab568467c04930
Rikki Gibson <rikkigibson@gmail.com>
2014-11-15 13:41:36 -0800

    laying groundwork for announcement detail view and getting data from parse

--- Hungry Students/ViewController.m
+++ Hungry Students/ViewController.m
@@ -7,11 +7,13 @@
 //
 
 #import "ViewController.h"
+#import <UIKit/UIKit.h>
 #import <Parse/Parse.h>
 #import <GoogleMaps/GoogleMaps.h>
 
 @interface ViewController ()
 @property (weak, nonatomic) IBOutlet GMSMapView *mapView;
+@property NSMutableArray *mapMarkers;
 
 @end
 
@@ -20,14 +22,51 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    self.mapMarkers = [NSMutableArray array];
+    
+    self.mapView.delegate = self;
     [self.mapView setCamera:[GMSCameraPosition cameraWithLatitude:44.56802
                                                             longitude:-123.27926
                                                              zoom:16]];
-    
-    GMSMarker *marker = [GMSMarker markerWithPosition:CLLocationCoordinate2DMake(44.56802, -123.27926)];
-    marker.title = @"Hello World";
-    marker.map = self.mapView;
     // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    
+    PFQuery *query = [PFQuery queryWithClassName:@"Announcement"];
+        
+    [query findObjectsInBackgroundWithBlock:^(NSArray *announcements, NSError *error) {
+        for (PFObject *announcement in announcements) {
+            [self.mapMarkers addObject:[self markerWithAnnouncement:announcement]];
+        }
+    }];
+}
+
+- (GMSMarker *)markerWithAnnouncement:(PFObject *)announcement {
+    
+    PFGeoPoint *location = announcement[@"Location"];
+    GMSMarker *result = [GMSMarker markerWithPosition:
+                         CLLocationCoordinate2DMake(location.latitude, location.longitude)];
+    
+    NSData *imageData = [announcement[@"Photo"] getData];
+    UIImage *photo = [UIImage imageWithData:imageData];
+    result.userData = @{ @"Photo" : photo, @"Description" : announcement[@"Description"] };
+    result.map = self.mapView;
+    
+    return result;
+}
+
+- (BOOL)mapView:(GMSMapView *)mapView didTapMarker:(GMSMarker *)marker {
+    UIViewController *destination = [UIViewController new];
+    
+    NSDictionary *data = marker.userData;
+    UIImage *photo = data[@"Photo"];
+    
+    destination.view = [[UIImageView alloc] initWithImage:photo];
+    [self.navigationController pushViewController:destination animated:true];
+    
+    return YES;
 }
 
 - (void)didReceiveMemoryWarning {
@@ -36,3 +75,4 @@
 }
 
 @end
+
