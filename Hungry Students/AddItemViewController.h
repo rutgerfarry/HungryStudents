65bf5772d296afcfdd5455a555665661df260133
Rutger Farry <rutgerfarry@gmail.com>
2014-11-15 13:40:28 -0800

    Image view in addItemViewController

---
+++ Hungry Students/AddItemViewController.h
@@ -0,0 +1,16 @@
+//
+//  AddItemViewController.h
+//  Hungry Students
+//
+//  Created by Rutger Farry on 11/15/14.
+//  Copyright (c) 2014 Rutger Farry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddItemViewController : UIViewController <UIImagePickerControllerDelegate, UINavigationControllerDelegate>
+
+@property (strong, nonatomic)UIImagePickerController *imagePicker;
+@property (strong, nonatomic)UIImage *image;
+
+@end
